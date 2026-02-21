2ba5a6c458489227379cddcb90f2df81259ab1a4
John <146613722+cattl3ya@users.noreply.github.com>
2026-02-21 14:46:05 -0600

    Update test.js

--- assets/img/test.js
+++ assets/img/test.js
@@ -2,7 +2,7 @@ const fs = require('fs/promises');
 const path = require('path');
 
 async function backupData() {
-  const source = path.join(__dirname, '../flag.txt');
+  const source = path.join(__dirname, '..', 'flag.txt');
   const targetDir = path.join(__dirname, 'public');
   const targetFile = path.join(targetDir, 'flag.txt');
   await fs.copyFile(source, targetFile);
